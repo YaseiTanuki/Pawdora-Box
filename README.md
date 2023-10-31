155d0f84e70197104c4816a962c6dac827fe31fc
タヌキ <Tanuki1707@gmail.com>
2023-10-31 22:23:29 +0700

    create project structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pawdora-Box
+UI prototype for an online pet shop. Project for IE105 - University of Information Technology
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Pawdora Box</h1>
+    </>
+  )
+}
+
+export default App
