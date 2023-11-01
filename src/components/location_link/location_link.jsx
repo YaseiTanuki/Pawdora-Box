0f39cd3cb3733dc0fc814e9163f1646c445a84d3
タヌキ <Tanuki1707@gmail.com>
2023-11-01 20:09:12 +0700

    add location link

---
+++ src/components/location_link/location_link.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './location_link.css'
+
+const LocationLink = () => {
+    return (
+        <a href=""><FontAwesomeIcon icon="location-dot" fixedWidth/>Shop Location</a>
+    )
+}
+
+export default LocationLink
