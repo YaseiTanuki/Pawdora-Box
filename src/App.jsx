f8b6d04b67049a6ee115661e73c6ab659ebee1b2
タヌキ <Tanuki1707@gmail.com>
2023-11-02 00:07:15 +0700

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import "./App.css"
+import "./App.scss"
 import { Routes, Route } from "react-router-dom"
 import "./utils/FontAwesome/font_awesome"
+import Header from "./components/big_components/header/header"
+
+//pages
 import Home from "./pages/home/home"
+import CatPage from "./pages/cat_page/cat_page"
 
 function App() {
   return (
     <>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/cat" element={<CatPage/>}/>
       </Routes>
     </>
   )
