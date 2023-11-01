f24a60e6b03f4db36c8bba7c7582de5b61e31915
タヌキ <Tanuki1707@gmail.com>
2023-11-01 20:09:50 +0700

    display location link

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import "./utils/FontAwesome/font_awesome"
+import LocationLink from "./components/location_link/location_link.jsx"
 
 function App() {
   return (
     <>
+      <LocationLink/>
       <h1>Pawdora Box</h1>
     </>
   )
