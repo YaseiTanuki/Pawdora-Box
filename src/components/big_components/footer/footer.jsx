301c8a44c6ec43ba9fc01481743eb027267c269d
タヌキ <Tanuki1707@gmail.com>
2023-11-02 22:40:22 +0700

    add footer infomation to footer

--- src/components/big_components/footer/footer.jsx
+++ src/components/big_components/footer/footer.jsx
@@ -1,17 +1,22 @@
 import "./footer.scss"
 import Logo from "../../small_components/logo/logo"
 import LocationLink from "../../small_components/location_link/location_link"
+import FooterInfo from "../../small_components/footer_info/footer_info"
 
 const Footer = () => {
     return (
         <footer>
-            <section>
+            <section className="footer-overall">
                 <div className="footer-logo-container">
                     <Logo/>
                 </div>
                 <div className="footer-location-container">
                     <LocationLink/>
                 </div>
+            </section>
+
+            <section className="footer-info">
+                <FooterInfo/>
             </section>
         </footer>
     )
