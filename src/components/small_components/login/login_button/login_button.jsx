0663aa19d7e6ad30433e3eb6c996d3a06f7faf2e
タヌキ <Tanuki1707@gmail.com>
2023-11-02 11:59:52 +0700

    add login button

---
+++ src/components/small_components/login/login_button/login_button.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./login_button.css"
+
+const LoginButton = () => {
+    return (
+        <div className="login-button">
+            <FontAwesomeIcon icon="right-to-bracket"/>
+        </div>
+    )
+}
+
+export default LoginButton
