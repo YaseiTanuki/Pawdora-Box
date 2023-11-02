b48929fa9cc68a5a5d2732e38c76bedd54c95410
タヌキ <Tanuki1707@gmail.com>
2023-11-02 19:01:48 +0700

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.scss"
 import { Routes, Route } from "react-router-dom"
 import "./utils/FontAwesome/font_awesome"
 import Header from "./components/big_components/header/header"
-
+import Footer from "./components/big_components/footer/footer"
 //pages
 import Home from "./pages/home/home"
 import CatPage from "./pages/cat_page/cat_page"
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/cat" element={<CatPage/>}/>
       </Routes>
+      <Footer/>
     </>
   )
 }
