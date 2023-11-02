0721632f7f9df9368bbd6464df8c628d8425958b
タヌキ <Tanuki1707@gmail.com>
2023-11-02 22:39:45 +0700

    add footer infomation

---
+++ src/components/small_components/footer_info/footer_info.jsx
@@ -0,0 +1,22 @@
+import "./footer_info.scss"
+
+const FooterInfo = () => {
+    return (
+        <section className="finfo-container">
+            <article className="finfo-service">
+                <h4>SERVICE</h4>
+                <p>Info about</p>
+            </article>
+            <article className="finfo-shopping">
+                <h4>SHOPPING</h4>
+                <p>Info about</p>
+            </article>
+            <article className="finfo-about-us">
+                <h4>ABOUT US</h4>
+                <p>Info about</p>
+            </article>
+        </section>
+    )
+}
+
+export default FooterInfo
