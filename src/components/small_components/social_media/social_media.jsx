b2405fc1d78a319759f5ea9fef86612f56724014
タヌキ <Tanuki1707@gmail.com>
2023-11-05 21:32:20 +0700

    social media components

---
+++ src/components/small_components/social_media/social_media.jsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const SocialMedia = () => {
+    return (
+        <article>
+            <FontAwesomeIcon icon={["fab", "facebook"]}/>
+            <FontAwesomeIcon icon={["fab", "instagram"]}/>
+            <FontAwesomeIcon icon={["fab", "youtube"]}/>
+        </article>
+    )
+}
+
+export default SocialMedia
