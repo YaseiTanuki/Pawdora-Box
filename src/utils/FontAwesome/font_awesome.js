c50630e8f81ed4ca7ce982220401269681163263
タヌキ <Tanuki1707@gmail.com>
2023-11-05 21:27:02 +0700

    add social media logo

--- src/utils/FontAwesome/font_awesome.js
+++ src/utils/FontAwesome/font_awesome.js
@@ -1,4 +1,5 @@
 import { faLocationDot, faRightToBracket } from "@fortawesome/free-solid-svg-icons"
 import { library } from "@fortawesome/fontawesome-svg-core"
+import { faFacebook, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons"
 
-library.add(faLocationDot, faRightToBracket)
+library.add(faLocationDot, faRightToBracket, faFacebook, faInstagram, faYoutube)
