84f2a2bdb5c50c53eb323390810dbdb75639b409
タヌキ <Tanuki1707@gmail.com>
2023-11-06 13:45:22 +0700

    make each icon in new line

--- src/utils/FontAwesome/font_awesome.js
+++ src/utils/FontAwesome/font_awesome.js
@@ -2,4 +2,8 @@ import { faLocationDot, faRightToBracket } from "@fortawesome/free-solid-svg-ico
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { faFacebook, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons"
 
-library.add(faLocationDot, faRightToBracket, faFacebook, faInstagram, faYoutube)
+library.add(faLocationDot,
+            faRightToBracket,
+            faFacebook,
+            faInstagram,
+            faYoutube)
