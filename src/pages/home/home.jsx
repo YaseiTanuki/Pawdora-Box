3690544c94449c2422e92efd40dc4ed38cc89a6a
タヌキ <Tanuki1707@gmail.com>
2023-11-06 23:13:13 +0700

    add quick button to home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,26 @@
 import "./Home.scss"
 import Banner from "../../components/small_components/banner/banner"
+import QuickChoiceButton from "../../components/small_components/quick_choice_button/quick_choice_button"
 
 const Home = () => {
     return (
-        <Banner/>
+        <main className="home">
+            <section className="banner-container">
+                <Banner/>
+            </section>
+            <section className="main-content">
+                <article className="quick-choice">
+                    <ul className="quick-choice-list">
+                        <li><QuickChoiceButton name="Dog Food"/></li>
+                        <li><QuickChoiceButton name="Cat Food"/></li>
+                        <li><QuickChoiceButton name="Dog Toy"/></li>
+                        <li><QuickChoiceButton name="Cat Toy"/></li>
+                        <li><QuickChoiceButton name="Pet Service"/></li>
+                        <li><QuickChoiceButton name="Medical"/></li>
+                    </ul>
+                </article>
+            </section>
+        </main>
     )
 }
 
