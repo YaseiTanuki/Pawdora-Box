75be99210297ba482e2985796c3fa5810f9287ff
タヌキ <Tanuki1707@gmail.com>
2023-11-14 22:30:46 +0700

    add navigation bar show option

--- src/components/big_components/header/header.jsx
+++ src/components/big_components/header/header.jsx
@@ -4,7 +4,7 @@ import Logo from "../../small_components/logo/logo";
 import LoginButton from "../../small_components/login/login_button/login_button";
 import SearchBar from "../../small_components/search_bar/search_bar";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
             <section className="site-name">
@@ -12,7 +12,9 @@ const Header = () => {
                 <SearchBar/>
                 <LoginButton/>
             </section>
-            <NavigationBar/>
+            { props.showNav ? 
+            <NavigationBar/> : <></>
+            }
         </header>
     )
 }
