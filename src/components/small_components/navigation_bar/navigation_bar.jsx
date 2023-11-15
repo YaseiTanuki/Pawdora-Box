8cab8c508f4ab52a656bf3e8149f4b8ff53a55ff
タヌキ <Tanuki1707@gmail.com>
2023-11-15 21:52:18 +0700

    add drop down to nav bar

--- src/components/small_components/navigation_bar/navigation_bar.jsx
+++ src/components/small_components/navigation_bar/navigation_bar.jsx
@@ -1,12 +1,37 @@
 import "./navigation_bar.scss"
 import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+import Popup from "reactjs-popup"
 
 const NavigationBar = () => {
+
+    const CatSubMenu = () => {
+        return (
+            <article className="cat-sub-menu">
+                <ul>
+                    <li>Cat Food</li>
+                    <li>Cat Toy</li>
+                    <li>Cat Drink</li>
+                </ul>
+            </article>
+        )
+    }
+
     return (
+
         <nav>
             <ul>
-                <li><NavLink className="NavLink" to="/">Home</NavLink></li>
-                <li><NavLink className="NavLink" to="/cat">Cat</NavLink></li>
+                <li><NavLink className="NavLink" to="/">HOME</NavLink></li>
+                <li>
+                    <Popup trigger={<NavLink className="NavLink" to="#">CAT<FontAwesomeIcon icon="caret-down"/></NavLink>} >
+                        <CatSubMenu/>
+                    </Popup>
+                </li>
+                <li><NavLink className="NavLink" to="#">DOG<FontAwesomeIcon icon="caret-down"/></NavLink></li>
+                <li><NavLink className="NavLink" to="#">HAMSTER<FontAwesomeIcon icon="caret-down"/></NavLink></li>
+                <li><NavLink className="NavLink" to="#">RABBIT<FontAwesomeIcon icon="caret-down"/></NavLink></li>
+                <li><NavLink className="NavLink" to="#">BIRD<FontAwesomeIcon icon="caret-down"/></NavLink></li>
             </ul>
         </nav>
     )
