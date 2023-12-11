ce10252b186651241348c281b44d37c85d12f44a
タヌキ <Tanuki1707@gmail.com>
2023-12-11 22:48:48 +0700

    change product list layout

--- src/components/small_components/product_card/product_card.jsx
+++ src/components/small_components/product_card/product_card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./product-card.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ProductCard = (props) => {
     return (
@@ -8,6 +9,10 @@ const ProductCard = (props) => {
             <h4 title="product name">{props.name}</h4>
             <data value={props.price}>{props.price}$</data>
             <p>{props.description}</p>
+            <div className="product-cart-button">
+                <button className="buy-now-button">Mua ngay</button>
+                <button className="add-to-cart-button"><FontAwesomeIcon icon="cart-shopping" /></button>
+            </div>
         </div>
     )
 }
