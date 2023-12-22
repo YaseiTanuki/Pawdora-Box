3f2fc9ee9fa1a5c254f4d683bbd377c8c535a210
UmiiiChannn <tthaii1803@gmail.com>
2023-12-22 10:39:18 +0700

    hahaa

--- src/pages/thanks_page/thanks_page.jsx
+++ src/pages/thanks_page/thanks_page.jsx
@@ -3,6 +3,8 @@ import Footer from "../../components/big_components/footer/footer"
 import './thanks_page.scss'
 import { useNavigate } from "react-router-dom"
 import process3 from"../../assets/img/process3.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
 
 const ThanksPage = () => {
 
@@ -19,7 +21,9 @@ const ThanksPage = () => {
                 </div>
             <div>
                 <article className="message">
-                    <div><h1>Cảm ơn bạn đã đặt hàng</h1></div>
+                    <div className="icon">
+                <FontAwesomeIcon icon={faShoppingBag} /></div>
+                    <div><h1>Cảm ơn bạn đã đặt hàng!</h1></div>
                     <div><p>Bạn sẽ sớm nhận được email xác nhận và thông tin theo dõi trong hộp thư đến của mình.</p></div>
                     <div><button onClick={toHome}>Tiếp tục mua sắm</button></div>
                 </article>
